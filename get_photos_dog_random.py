488fc510dfa18b4d4f567970708ec57bba299f1b
Aleksandr <DedghhinAleksandrV@yandex.ru>
2026-08-25 22:29:26 +0500

    modified get_photos_dog_random.py,site_view.py,breed_voice.mp3,test_api_dog.json create

--- get_photos_dog_random.py
+++ get_photos_dog_random.py
@@ -1,15 +1,19 @@
 import requests
 from time import sleep
 from tqdm import tqdm
+import json
 
 def get_photos_random() :
-    '''Функция возвращает ссылку на случайное фото собаки'''
+    '''Функция возвращает ссылку на случайное фото собаки и название породы'''
     max_retries = 3
     delay = 2
     for retries in range(1,max_retries+1) :
         try:
             response_get_photo_url = requests.get('https://dog.ceo/api/breeds/image/random',timeout=4).json()['message']
-            return response_get_photo_url
+            breed = response_get_photo_url.split("/")
+            img_bytes = requests.get(response_get_photo_url).content
+            return (response_get_photo_url,breed[-2],img_bytes)
+        
         except (requests.exceptions.RequestException,KeyError) as e :
             print(f'Попытка {retries}/{max_retries} Ошибка сети {e}')
             if retries < max_retries :
@@ -17,13 +21,18 @@ def get_photos_random() :
                 sleep(delay)
             else:
                 print("Сервер не отвечает")
-                return "error"
+                return (None,None)
 
 if __name__ == "__main__" :
 
-    print("Старт теста")
-    progress_line = tqdm(range(1,5),desc="Скачивание любой породы",unit="шт")
-    for line in progress_line :
-        file = get_photos_random()
+    response_get_photo_url = requests.get('https://dog.ceo/api/breeds/image/random',timeout=4).json()['message']
+    breed = response_get_photo_url.split("/")
+    print(breed[-2])
+    #with open("test_api_dog.json",'w',encoding='utf-8') as file :
+    #    json.dump(response_get_photo_url,file,indent=4,ensure_ascii=False)
+    #print("Старт теста")
+    #progress_line = tqdm(range(1,5),desc="Скачивание любой породы",unit="шт")
+    #for line in progress_line :
+    #    file = get_photos_random()
         
-    print("\nТест завершен")
+    #print("\nТест завершен")
--- site_view.py
+++ site_view.py
@@ -111,7 +111,8 @@ def site_menu_interface(db) :
                 options=[
                     "Выбор из 4х вариантов",
                     "Письменный тест (ввести правильное слово)",
-                    "Напиши имя героя"
+                    "Напиши имя героя",
+                    "Напиши породу собаки"
                 ],
                 horizontal=True
             )
@@ -204,9 +205,6 @@ def site_menu_interface(db) :
                     if clicked_button is not None :
                         if clicked_button == card.words_english :
                             st.success("Вы ответили правильно")
-                            dog_photo = get_photos_random()
-                            if dog_photo != "error" :
-                                st.image(dog_photo)
                             db.get_and_update_status(st.session_state.current_user_id,card.word_id,is_correct=True)
                         else :
                             st.error(f"Ошибка Верный ответ {card.words_english}")
@@ -279,9 +277,6 @@ def site_menu_interface(db) :
                     if check_button.button("Проверить ответ",use_container_width=True):
                         if user_answer == card.words_english.strip().lower() :
                             st.success("Вы ответили правильно")
-                            dog_photo = get_photos_random()
-                            if dog_photo != "error" :
-                                st.image(dog_photo)
                             db.get_and_update_status(st.session_state.current_user_id,card.word_id,is_correct=True)
                         else :
                             st.warning("Попробуйте еще")
@@ -380,6 +375,98 @@ def site_menu_interface(db) :
                         st.session_state.current_hero = None
                         st.session_state.answer_correct = False
                         st.rerun()
+
+            elif study_choise == "Напиши породу собаки" :
+                st.subheader("Порода Собаки")
+                if "current_dog_url" not in st.session_state or st.session_state.current_dog_url is None :
+                    with st.spinner("Выбираем породу") :
+                        while True :
+                            breed_url,breed_name,breed_bytes = get_photos_random()
+                            if breed_url and breed_name :
+                                st.session_state.current_dog_url = breed_url
+                                st.session_state.current_dog_name = breed_name.replace("-"," ").strip()
+                                st.session_state.wrong_attempts = 0
+                                st.session_state.current_breed_bytes = breed_bytes
+                                break
+                if st.session_state.current_dog_url :
+                    st.image(st.session_state.current_breed_bytes,width=200)
+                    st.write("---")
+                    st.markdown("### Вы можете прослушать произношение")
+                    local_audio_path = "breed_voice.mp3"
+                    try :
+                        tts = gTTS(text=st.session_state.current_dog_name,lang="en")
+                        tts.save(local_audio_path)
+                        with open(local_audio_path,"rb") as audio_file :
+                            audio_b = audio_file.read() 
+                        st.audio(audio_b,format="audio/mp3")
+                    except Exception as e :
+                        st.error("Не удалось сформировать озвучку")
+                        print(f"Ошибка gTTS {e}")
+                    user_input = st.text_input("Введите название породы").lower().strip()
+                    if "answer_correct" not in st.session_state :
+                        st.session_state.answer_correct = False
+                    if not st.session_state.answer_correct :
+                        check_button = st.button("Проверить ответ",use_container_width=True)
+                        skip_button = st.button("Следующий герой",use_container_width=True)
+                        if check_button :
+                            clear_string1 = sub(r"[^a-z]+",'',st.session_state.current_dog_name).lower()
+                            clear_string2 = sub(r"[^a-z]+",'',user_input)
+                                
+                            if clear_string1 == clear_string2 :
+                                st.session_state.answer_correct = True
+                                st.rerun()
+                            else :
+                                st.session_state.wrong_attempts += 1
+                                if st.session_state.wrong_attempts >= 3 :
+                                    st.error(f"Вы ответили не правильно 3 раза ")
+                                    st.info(f"Название породы ***{st.session_state.current_dog_name} ***")
+                                else :
+                                    st.error(f"Ошибка. Попробуйте еще раз Попытка {st.session_state.wrong_attempts} из 3")
+                        elif skip_button :
+                            st.session_state.current_dog_url = None 
+                            st.session_state.current_dog_name = None
+                            st.session_state.wrong_attempts = 0
+                            st.rerun()
+                    else :
+                        st.success(f"Вы ввели верную породу {st.session_state.current_dog_name}")
+                        save_mode = st.radio(
+                                "Выберите способ сохранения картинки",
+                                ["На Яндекс диск","На компьютер"],
+                                horizontal=True
+                            )
+                        save_picture = st.button("Сохранить картинку",use_container_width=True)
+                        if save_picture :
+                            try :
+                                if st.session_state.current_dog_url and st.session_state.current_dog_url.startswith("http"):
+                                    with st.spinner("Скачиваем картинку из сети ") :
+                                        filename = f"{st.session_state.current_dog_name.replace(" ","-")}.jpg"
+            
+                                        if save_mode == "На Яндекс диск" :
+                                            with st.spinner("Загрузка картинки на облако"):
+                                                cloud_path = f"disk:/Dog/{filename}"
+                                                sleep(1)
+                                                save_dog_picture = YandexDiskService(y_token=ydt)
+                                                res = save_dog_picture.send_file(st.session_state.current_breed_bytes,cloud_path)
+                                            if res == 201 or res == 200 :
+                                                st.info("Картинка успешно сохранена на Яндекс диск")
+                                            else :
+                                                st.error(f"Ошибка загрузки статус ответа яндекса {res}")
+                                        else :
+                                            with st.spinner("Загрузка картинки на компьютер") :
+                                                local_folder = "downloads"
+                                                os.makedirs(local_folder,exist_ok=True)
+                                                local_path = os.path.join(local_folder,filename)
+                                                with open(local_path,"wb") as file :
+                                                    file.write(st.session_state.current_breed_bytes)
+                                                sleep(1)
+                                                st.info("Картинка успешно сохранена на компьютер")
+                            except Exception as e :
+                                st.error(f"Не удалось сохранить файл {e}")
+                        if st.button("Следующая порода",key="next_after_success",use_container_width=True) :
+                            st.session_state.current_dog_url = None 
+                            st.session_state.current_dog_name = None
+                            st.session_state.answer_correct = False
+                            st.rerun()            
                 
 
         case "add" :
